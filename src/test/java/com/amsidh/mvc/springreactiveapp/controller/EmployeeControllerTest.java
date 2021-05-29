8a038278e903cd1a766dab315995b48aed43d733
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-05-29 23:19:06 +0530

    Added Junit for CreateEmployee method

--- src/test/java/com/amsidh/mvc/springreactiveapp/controller/EmployeeControllerTest.java
+++ src/test/java/com/amsidh/mvc/springreactiveapp/controller/EmployeeControllerTest.java
@@ -1,5 +1,6 @@
 package com.amsidh.mvc.springreactiveapp.controller;
 
+import com.amsidh.mvc.springreactiveapp.exception.EmployeeNotFoundException;
 import com.amsidh.mvc.springreactiveapp.model.EmployeeVO;
 import com.amsidh.mvc.springreactiveapp.service.EmployeeService;
 import org.junit.jupiter.api.Assertions;
@@ -11,13 +12,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.util.Base64Utils;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
+
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.csrf;
 
 @ExtendWith(SpringExtension.class)
 @WebFluxTest(controllers = EmployeeController.class)
@@ -41,11 +49,57 @@ public class EmployeeControllerTest {
                         .encodeToString(("user" + ":" + "password").getBytes(StandardCharsets.UTF_8)))
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(EmployeeVO.class).value(employeeVO1 -> Assertions.assertEquals(employeeVO.getEmail(), employeeVO.getEmail()));
-
+                .expectBody(EmployeeVO.class)
+                .value(empVO -> Assertions.assertEquals(employeeVO.getEmail(), empVO.getEmail()));
         Mockito.verify(employeeService, Mockito.times(1)).getEmployee(ArgumentMatchers.any(UUID.class));
-
     }
 
+    @Test
+    public void testGetEmployeeByIdNotFound() {
+        UUID uuid = UUID.randomUUID();
+        Mockito.when(employeeService.getEmployee(ArgumentMatchers.any(UUID.class)))
+                .thenThrow(new EmployeeNotFoundException(uuid));
+        webClient.get().uri("/employees/{employeeId}", uuid)
+                .header("Authorization", "Basic " + Base64Utils
+                        .encodeToString(("user" + ":" + "password").getBytes(StandardCharsets.UTF_8)))
+                .exchange()
+                .expectStatus().isNotFound();
+        Mockito.verify(employeeService, Mockito.times(1)).getEmployee(ArgumentMatchers.any(UUID.class));
+    }
+
+    @Test
+    public void testFindAllEmployee() {
+        List<EmployeeVO> employeeVOList = Arrays.asList(EmployeeVO.builder().name("Amsidh Lokhande").email("amsidh@gmail.com").build(),
+                EmployeeVO.builder().name("Adithi Lokhande").email("adithi@gmail.com").build(),
+                EmployeeVO.builder().name("Aditya Lokhande").email("aditya@gmail.com").build());
+        Mockito.when(employeeService.getEmployees()).thenReturn(Flux.fromIterable(employeeVOList));
+
+        webClient.get().uri("/employees")
+                .header("Authorization", "Basic " + Base64Utils
+                        .encodeToString(("user" + ":" + "password").getBytes(StandardCharsets.UTF_8)))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(new ParameterizedTypeReference<List<EmployeeVO>>() {
+                })
+                .value(emp -> Assertions.assertFalse(emp.isEmpty()));
+
+        Mockito.verify(employeeService, Mockito.times(1)).getEmployees();
+    }
+
+    @Test
+    public void testPostEmployee() {
+        EmployeeVO employeeVO = EmployeeVO.builder().id(UUID.randomUUID()).name("Amsidh Lokhande").email("amsidh@gmail.com").build();
+        Mockito.when(employeeService.createEmployee(ArgumentMatchers.any(EmployeeVO.class))).thenReturn(Mono.just(employeeVO));
+        //CSRF Token has been associated to this client. So using webClient.mutateWith(csrf())
+        webClient.mutateWith(csrf()).post().uri("/employees")
+                .header("Authorization", "Basic " + Base64Utils
+                        .encodeToString(("user" + ":" + "password").getBytes(StandardCharsets.UTF_8)))
+                .body(BodyInserters.fromValue(EmployeeVO.builder().name("Amsidh Lokhande").email("amsidh@gmail.com").build()))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(EmployeeVO.class)
+                .value(empVo -> Assertions.assertEquals(empVo.getName(), employeeVO.getName()));
+        Mockito.verify(employeeService, Mockito.times(1)).createEmployee(ArgumentMatchers.any(EmployeeVO.class));
+    }
 
 }
