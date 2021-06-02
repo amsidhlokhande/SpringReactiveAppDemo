196ccb7efcf46090560ed0a3097cbd31a353a846
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-06-02 15:30:37 +0530

    EmployeeHandler Lambda function applied wherever possible

--- src/main/java/com/amsidh/mvc/springreactiveapp/handler/EmployeeHandler.java
+++ src/main/java/com/amsidh/mvc/springreactiveapp/handler/EmployeeHandler.java
@@ -8,9 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.HandlerFunction;
-import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
@@ -23,72 +21,40 @@ public class EmployeeHandler {
 
     public HandlerFunction<ServerResponse> findAllEmployees() {
         log.info("EmployeeHandler findAllEmployees method called");
-        return new HandlerFunction<ServerResponse>() {
-            @Override
-            public Mono<ServerResponse> handle(ServerRequest serverRequest) {
-                return ServerResponse.ok().body(employeeService.getEmployees(), EmployeeVO.class);
-            }
-        };
+        return serverRequest -> ServerResponse.ok().body(employeeService.getEmployees(), EmployeeVO.class);
     }
 
     public HandlerFunction<ServerResponse> findEmployeeById() {
         log.info("EmployeeHandler findEmployeeById method called");
-        return new HandlerFunction<ServerResponse>() {
-            @Override
-            public Mono<ServerResponse> handle(ServerRequest serverRequest) {
-                return ServerResponse.ok().body(employeeService.getEmployee(UUID.fromString(serverRequest.pathVariable("employeeId"))), EmployeeVO.class);
-            }
-        };
+        return serverRequest -> ServerResponse.ok().body(employeeService.getEmployee(UUID.fromString(serverRequest.pathVariable("employeeId"))), EmployeeVO.class);
     }
 
     public HandlerFunction<ServerResponse> postEmployee() {
         log.info("EmployeeHandler postEmployee method called");
-        return new HandlerFunction<ServerResponse>() {
-            @Override
-            public Mono<ServerResponse> handle(ServerRequest serverRequest) {
-                return serverRequest.bodyToMono(EmployeeVO.class).flatMap(employeeVO -> {
-                    return ServerResponse.ok().body(employeeService.createEmployee(employeeVO), EmployeeVO.class);
-                });
-            }
-        };
+        return serverRequest -> serverRequest.bodyToMono(EmployeeVO.class).flatMap(employeeVO -> ServerResponse.ok().body(employeeService.createEmployee(employeeVO), EmployeeVO.class));
     }
 
     public HandlerFunction<ServerResponse> updateEmployee() {
         log.info("EmployeeHandler findEmployeeById method called");
-        return new HandlerFunction<ServerResponse>() {
-            @Override
-            public Mono<ServerResponse> handle(ServerRequest serverRequest) {
-                return ServerResponse
-                        .ok()
-                        .body(employeeService.updateEmployee(UUID
-                                        .fromString(serverRequest.pathVariable("employeeId")),
-                                serverRequest.bodyToMono(EmployeeVO.class)),
-                                EmployeeVO.class);
-            }
-        };
+        return serverRequest -> ServerResponse
+                .ok()
+                .body(employeeService.updateEmployee(UUID
+                                .fromString(serverRequest.pathVariable("employeeId")),
+                        serverRequest.bodyToMono(EmployeeVO.class)),
+                        EmployeeVO.class);
     }
 
     public HandlerFunction<ServerResponse> deleteEmployeeById() {
         log.info("EmployeeHandler findEmployeeById method called");
-        return new HandlerFunction<ServerResponse>() {
-            @Override
-            public Mono<ServerResponse> handle(ServerRequest serverRequest) {
-                return ServerResponse.ok().body(employeeService.deleteEmployee(UUID
-                        .fromString(serverRequest.pathVariable("employeeId"))), Void.class);
-            }
-        };
+        return serverRequest -> ServerResponse.ok().body(employeeService.deleteEmployee(UUID
+                .fromString(serverRequest.pathVariable("employeeId"))), Void.class);
     }
 
     public HandlerFunction<ServerResponse> getEmployeeByPagination() {
         log.info("EmployeeHandler getEmployeeByPagination method called");
-        return new HandlerFunction<ServerResponse>() {
-            @Override
-            public Mono<ServerResponse> handle(ServerRequest serverRequest) {
-                return ServerResponse.ok().body(employeeService.getEmployeePaging(
-                        serverRequest.queryParam("name").orElse(""), serverRequest.queryParam("email").orElse(""), PageRequest.of(
-                                Integer.parseInt(serverRequest.queryParam("pageNumber").orElse("0")), Integer.parseInt(serverRequest.queryParam("pageSize").orElse("20")))
-                ), EmployeePageList.class);
-            }
-        };
+        return serverRequest -> ServerResponse.ok().body(employeeService.getEmployeePaging(
+                serverRequest.queryParam("name").orElse(""), serverRequest.queryParam("email").orElse(""), PageRequest.of(
+                        Integer.parseInt(serverRequest.queryParam("pageNumber").orElse("0")), Integer.parseInt(serverRequest.queryParam("pageSize").orElse("20")))
+        ), EmployeePageList.class);
     }
 }
